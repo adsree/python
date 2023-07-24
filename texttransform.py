d8ce346ae347b95b8e73f49eb42ca43a9fcc923c
mubarack <mmmpandme7736@gmail.com>
2023-07-24 16:11:22 +0530

    22/7

--- texttransform.py
+++ texttransform.py
@@ -2,8 +2,8 @@ def capital(string):
   words = string.split()
   capital = []
   for i in words:
-     capital_word = i[0].upper() + i[1:]
-     capital.append(capital_word)
+    capital_word = i[0].upper() + i[1:]
+    capital.append(capital_word)
 
   capital_string = " ".join(capital)
 

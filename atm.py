1364ca5943ff9990b1ddf6bd501970178efd12f3
mubarack <mmmpandme7736@gmail.com>
2023-07-27 16:20:53 +0530

    28/7

--- atm.py
+++ atm.py
@@ -7,20 +7,24 @@ def withdraw_amt(amt):
     withd=float(input("Enter the amount you want to withdraw:"))
     if(amt>withd):
         amt-=withd
-        print("Withdrawal successful. Your current account balance is" ,amt)
+        return amt
     else:
         print("\nInsufficient balance!")
+        exit()
 
+amount=float(10000)
 while (True):
-    amount=float(10000)
+    
     print("1: Account Balance \n2: Money Deposit \n3: Money Withdrawal \n4: Exit")
     choice=int(input("Enter your choice:"))
     if(choice==1):
         print("Your current bank balance is:",amount)
     elif(choice==2):
-        print("Deposit successful. Your current account balance is",deposit_amt(amount))
+        amount = deposit_amt(amount)
+        print("Deposit successful. Your current account balance is",amount)
     elif(choice==3):
-        withdraw_amt(amount)
+        amount = withdraw_amt(amount)
+        print("Withdrawal successful. Your current account balance is" ,amount)
     elif(choice==4):
         print("Exiting")
         break

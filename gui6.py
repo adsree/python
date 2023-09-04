87367e86b389a7c54a7f7b1212073b3d82384518
muhammadshiblii <pmuhammadshibli@gmail.com>
2023-09-04 15:53:56 +0530

    dbcfn

--- gui6.py
+++ gui6.py
@@ -2,18 +2,22 @@ from tkinter import *
 master = Tk()
 
 
-label1=Label.grid(sticky=E)
-label2=Label.grid(sticky=E) 
+label1=Label(text="Enter first number")
+label1.grid(sticky=E)
+e1=Entry()
+e1.grid(row=0, column=1)
+label2=Label(text="Enter second number")
+label2.grid(sticky=E)
+e2=Entry()
+e2.grid(row=1, column=1)
 
-e1=Entry.grid(row=0, column=1)
-e2=Entry.grid(row=1, column=1)
+e3=Entry(text="0")
+e3.grid(row=3, column=1)
 
-Checkbutton.grid(columnspan=2,sticky=W)
-
-Image.grid(row=0,column=2,columnspan=2,rowspan=2,sticky=W+E+N+S, padx=5, pady=5)
-
-
-Button.grid(row=2, column=2)
-Button.grid(row=2, column=3)
-
-mainloop()
+def add():
+    sum= (int(e1.get())+int(e2.get()))
+    e3.delete(0,END)
+    e3.insert(END,sum)
+b1=Button(text="Add", command=add)
+b1.grid(row=2, column=1)
+master.mainloop()
